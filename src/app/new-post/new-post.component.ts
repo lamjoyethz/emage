5903d6100a7749211722b48eb50754fb53c222e7
Satellarknight <88333750+Satellarknight@users.noreply.github.com>
2023-11-13 22:27:39 +0100

    Update new-post.component.ts

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-new-post',
@@ -6,6 +9,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-post.component.scss'],
 })
 export class NewPostComponent {
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+  
+
+  sliderValue: number = 1;
+  
+  showSlider: boolean = false;
+
+  /* I guess value is also not stored */
+  setSelectedValue(value: number, option: string): void {
+    switch (option) {
+      case 'stress':
+        this.sliderValue = value;
+        break;
+      case 'hungry':
+        this.sliderValue = value;
+        break;
+      case 'tiredness':
+        this.sliderValue = value;
+        break;
+      default:
+        break;
+    }
+    this.showSlider = true;
+    this.cdRef.detectChanges();
+  }
+
+  /* backgroundcolor not done */
+  getButtonBackgroundColor(): string {
+    let value = this.sliderValue;
+    if (value === 1) {
+      return 'transparent';
+    } else if (value === 2) {
+      return 'cyan';
+    } else if (value === 3) {
+      return 'green';
+    } else if (value === 4) {
+      return 'yellow';
+    } else if (value === 5) {
+      return 'pink';
+    } else {
+      return 'transparent';
+    }
+  }
+
   onClick() {
     // read username
     const storedUsername = localStorage.getItem('userName');
@@ -30,3 +78,4 @@ export class NewPostComponent {
       });
   }
 }
+
