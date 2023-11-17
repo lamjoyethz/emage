5dff4404783556a175f9138e8d46cbb300383fa2
tuichi <lamjoy@ethz.ch>
2023-11-17 18:11:11 +0100

    hallo Tai

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -6,86 +6,110 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-post.component.scss'],
 })
 export class NewPostComponent {
+  sliderValue1: number = 1;
+  showSlider1: boolean = false;
+  buttonColor1 = 'cyan'; // Default color
 
-  sliderValues: { [key: string]: number } = {
-    stress: 1,
-    hungry: 1,
-    tiredness: 1,
-    sports: 1,
-    romance: 1,
-    work: 1,
-    angry: 1,
-    happy: 1,
-    sad: 1,
-    // Add other emotions and their default values here
-  };
+  sliderValue2: number = 1;
+  showSlider2: boolean = false;
+  buttonColor2 = 'cyan'; // Default color
 
-  activeButton: string | null = null;
-
-  setSelectedValue(value: number, emotion: string): void {
-    this.activeButton = emotion;
-    // Set the value of the selected slider
-    this.sliderValues[emotion] = value;
-  }
-
-  isSliderVisible(emotion: string): boolean {
-    return this.activeButton === emotion;
-  }
-
-  onSliderChange(emotion: string) {
-    console.log(`Slider value for ${emotion}: ${this.sliderValues[emotion]}`);
-    console.log('Slider value:', this.sliderValues[emotion]);
-    this.buttonBgColor = this.getButtonBackgroundColor(emotion);
-  }
-
-  sliderValue: number = 1;
-
-  showSlider: boolean = false;
-
-  buttonBgColor: string = 'transparent';
-
-  previousSliderValue: number = 1; // 用于保存上一个滑块值
-
-  /* I guess value is also not stored 
-  setSelectedValue(value: number, option: string): void {
-    switch (option) {
-      case 'stress':
-        this.sliderValue = value;
-        break;
-      case 'hungry':
-        this.sliderValue = value;
-        break;
-      case 'tiredness':
-        this.sliderValue = value;
-        break;
-      default:
-        break;
-    }
-    this.showSlider = true;
-  }
-  */
-
-  
-
-  /* backgroundcolor not done */
-  getButtonBackgroundColor(emotion: string): string {
-    // 根据滑块的值返回相应的颜色
-    switch (this.sliderValues[emotion]) {
-      case 1:
-        return 'cyan';
-      case 2:
-        return 'cyan';
-      case 3:
-        return 'green';
-      case 4:
-        return 'yellow';
-      case 5:
-        return 'pink';
-      // 添加更多滑块值和对应颜色
-      default:
-        return 'transparent';
+  triggerButton(pos: number) {
+    if (pos == 1) {
+      this.showSlider1 = !this.showSlider1;
+    } else if (pos == 2) {
+      this.showSlider2 = !this.showSlider2;
+    } else {
     }
   }
+
+  updateButtonColor() {
+    if (this.sliderValue1 == 1) {
+      this.buttonColor1 = 'cyan';
+    } else if (this.sliderValue1 == 2) {
+      this.buttonColor1 = 'orange';
+    } else {
+      this.buttonColor1 = 'red';
+    }
+  }
+
+  // sliderValues: { [key: string]: number } = {
+  //   stress: 1,
+  //   hungry: 1,
+  //   tiredness: 1,
+  //   sports: 1,
+  //   romance: 1,
+  //   work: 1,
+  //   angry: 1,
+  //   happy: 1,
+  //   sad: 1,
+  //   // Add other emotions and their default values here
+  // };
+
+  // activeButton: string | null = null;
+
+  // setSelectedValue(value: number, emotion: string): void {
+  //   this.activeButton = emotion;
+  //   // Set the value of the selected slider
+  //   this.sliderValues[emotion] = value;
+  // }
+
+  // isSliderVisible(emotion: string): boolean {
+  //   return this.activeButton === emotion;
+  // }
+
+  // onSliderChange(emotion: string) {
+  //   console.log(`Slider value for ${emotion}: ${this.sliderValues[emotion]}`);
+  //   console.log('Slider value:', this.sliderValues[emotion]);
+  //   this.buttonBgColor = this.getButtonBackgroundColor(emotion);
+  // }
+
+  // sliderValue: number = 1;
+
+  // showSlider: boolean = false;
+
+  // buttonBgColor: string = 'transparent';
+
+  // previousSliderValue: number = 1; // 用于保存上一个滑块值
+
+  // /* I guess value is also not stored
+  // setSelectedValue(value: number, option: string): void {
+  //   switch (option) {
+  //     case 'stress':
+  //       this.sliderValue = value;
+  //       break;
+  //     case 'hungry':
+  //       this.sliderValue = value;
+  //       break;
+  //     case 'tiredness':
+  //       this.sliderValue = value;
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  //   this.showSlider = true;
+  // }
+  // */
+
+  // /* backgroundcolor not done */
+  // getButtonBackgroundColor(emotion: string): string {
+  //   // 根据滑块的值返回相应的颜色
+  //   switch (this.sliderValues[emotion]) {
+  //     case 1:
+  //       return 'cyan';
+  //     case 2:
+  //       return 'cyan';
+  //     case 3:
+  //       return 'green';
+  //     case 4:
+  //       return 'yellow';
+  //     case 5:
+  //       return 'pink';
+  //     // 添加更多滑块值和对应颜色
+  //     default:
+  //       return 'transparent';
+  //   }
+  // }
 
   onClick() {
     // read username
